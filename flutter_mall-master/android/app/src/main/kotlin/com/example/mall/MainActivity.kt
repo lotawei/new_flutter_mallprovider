b9d6cd1a1a839194bbeaa54f737b5e7cc620e1ce
lotawei <1286594907@qq.com>
2023-06-11 16:20:56 +0800

    lotawei
    fix essues

--- flutter_mall-master/android/app/src/main/kotlin/com/example/mall/MainActivity.kt
+++ flutter_mall-master/android/app/src/main/kotlin/com/example/mall/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.example.mall
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-
+import io.flutter.embedding.android.FlutterActivity;
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
